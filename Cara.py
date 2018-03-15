86159d3428d8272e855acd3563f05511f9720018
Zifan94 <mikeyang1625@gmail.com>
2018-03-14 20:28:14 -0400

    apply to use

--- Cara.py
+++ Cara.py
@@ -21,6 +21,19 @@ def calc_0_to_29(sampleCount, all_history_price):    # [0] to [29]  price_day-1 
     return X
 
 def get_Date(datapath):                 #read date
+    pred_date =None
+    with open(datapath,'r') as csvfile:
+        reader = csv.reader(csvfile)
+        Date = [row[0] for row in reader]
+    for i in range(0,len(Date)):
+        Date[i]=Date[i].replace("/","-")
+        Date[i]=datetime.datetime.strptime(Date[i], "%Y-%m-%d")
+        pred_date = Date[i]
+#    print(Date[0])
+    pred_date = pred_date+datetime.timedelta(days=1)
+    return Date, pred_date
+
+def get_ER_Date(datapath):                 #read date
     with open(datapath,'r') as csvfile:
         reader = csv.reader(csvfile)
         Date = [row[0] for row in reader]
@@ -28,8 +41,7 @@ def get_Date(datapath):                 #read date
         Date[i]=Date[i].replace("/","-")
         Date[i]=datetime.datetime.strptime(Date[i], "%m-%d-%Y")
 #    print(Date[0])
-    return Date
-    
+    return Date    
 
 
 def calc_30_to_32(sampleCount, ER_Date, Date):
@@ -65,7 +77,8 @@ def calc_33_to_44(sampleCount, Date):
 if __name__ == "__main__":
     
     # dataPath = "Data/NVDA_TEST.csv"
-    dataPath = "Data/NVDA_ORG.csv"
+    # dataPath = "Data/NVDA_ORG.csv"
+    dataPath = "Data/NVDA.csv"
     nvdaDB = genfromtxt(dataPath, delimiter=',')
 
     all_history_price = nvdaDB[0:, 4]
@@ -77,61 +90,79 @@ if __name__ == "__main__":
     
      
     erdatePath='Data/NVDA_ER_Date.csv'
-    ERdate = get_Date(erdatePath)
-    date = get_Date(dataPath)
+    ERdate = get_ER_Date(erdatePath)
+    date, pred_date = get_Date(dataPath)
     train_X += calc_30_to_32(sampleCount, ERdate,date)
 
     train_X += calc_33_to_44(sampleCount, date)
     # get rid of first 400 example since its too long
-    train_X = train_X[400:, ]
-    train_Y = train_Y[400:, ]
+    offset = 3977
+    train_X = train_X[offset:, ]
+    train_Y = train_Y[offset:, ]
 
     # Train_X, Train_Y constructed
     np.savetxt("OUT-train_X.csv", train_X, fmt='%s', delimiter=",")
     np.savetxt("OUT-train_Y.csv", train_Y, fmt='%s', delimiter=",")
 
-    print("training example count: "+str(nvdaDB.shape[0]-30-400))
+    print("training example count: "+str(nvdaDB.shape[0]-30-offset))
     print("training data X: "+str(train_X.shape))
     print("training data Y: "+str(train_Y.shape))
-    assert nvdaDB.shape[0]-30-400 == train_X.shape[0]
-    assert nvdaDB.shape[0]-30-400 == train_Y.shape[0]
+    assert nvdaDB.shape[0]-30-offset == train_X.shape[0]
+    assert nvdaDB.shape[0]-30-offset == train_Y.shape[0]
 
     # Using Linear Regression
-    date_X = zeros((nvdaDB.shape[0]-30-400-1-500, 1))
-    date_cnt = 1
-    test_Y = zeros((nvdaDB.shape[0]-30-400-1-500, 1))
-    pred_Y = zeros((nvdaDB.shape[0]-30-400-1-500, 1))
-    for i in range(500, nvdaDB.shape[0]-30-400-1):
-        tmp_train_X = train_X[i:,]
-        tmp_train_Y = train_Y[i:,]
-        linearReg = LinearRegression()
-        linearReg.fit(tmp_train_X, tmp_train_Y)
-        tmp_test_X = train_X[i+1,]
-        tmp_test_Y = train_Y[i+1][0]
-        pred_Y[i-500][0] = (linearReg.predict(tmp_test_X.reshape(1, -1))[0][0])
-        test_Y[i-500][0] = (tmp_test_Y)
-        date_X[i-500][0] = date_cnt
-        date_cnt += 1
+    # date_X = zeros((nvdaDB.shape[0]-30-offset-1-500, 1))
+    # date_cnt = 1
+    # test_Y = zeros((nvdaDB.shape[0]-30-offset-1-500, 1))
+    # pred_Y = zeros((nvdaDB.shape[0]-30-offset-1-500, 1))
+    # for i in range(500, nvdaDB.shape[0]-30-offset-1):
+    #     tmp_train_X = train_X[i:,]
+    #     tmp_train_Y = train_Y[i:,]
+    #     linearReg = LinearRegression()
+    #     linearReg.fit(tmp_train_X, tmp_train_Y)
+    #     tmp_test_X = train_X[i+1,]
+    #     tmp_test_Y = train_Y[i+1][0]
+    #     pred_Y[i-500][0] = (linearReg.predict(tmp_test_X.reshape(1, -1))[0][0])
+    #     test_Y[i-500][0] = (tmp_test_Y)
+    #     date_X[i-500][0] = date_cnt
+    #     date_cnt += 1
 
-    sum = 0
-    for i in range(0, test_Y.shape[0]):
-        dis = test_Y[i][0] - pred_Y[i][0]
-        disSq = dis * dis
-        sum += disSq
-    MSE = sum / test_Y.shape[0]
-    print("")
-    print("Min Squared Error: "+str(round(sqrt(MSE), 2)))
-    print("")
+    # sum = 0
+    # for i in range(0, test_Y.shape[0]):
+    #     dis = test_Y[i][0] - pred_Y[i][0]
+    #     disSq = dis * dis
+    #     sum += disSq
+    # MSE = sum / test_Y.shape[0]
+    # print("")
+    # print("Min Squared Error: "+str(round(sqrt(MSE), 2)))
+    # print("")
 
-    print("dateCnt data X: "+str(date_X.shape))
-    print("testing data Y: "+str(test_Y.shape))
-    print("predict data Y: "+str(pred_Y.shape))
+    # print("dateCnt data X: "+str(date_X.shape))
+    # print("testing data Y: "+str(test_Y.shape))
+    # print("predict data Y: "+str(pred_Y.shape))
 
 
-    # Plot
-    fig, ax = plt.subplots()
-    plt.plot(date_X, test_Y, 'r', label = "target", lw=2)
-    plt.plot(date_X, pred_Y, 'b', label = "predict", lw=1)
-    plt.legend(loc = 0)
-    plt.show()
+    # # Plot
+    # fig, ax = plt.subplots()
+    # plt.plot(date_X, test_Y, 'r', label = "target", lw=2)
+    # plt.plot(date_X, pred_Y, 'b', label = "predict", lw=1)
+    # plt.legend(loc = 0)
+    # plt.show()
 
+
+    # Actual predict
+    linearReg = LinearRegression()
+    linearReg.fit(train_X, train_Y)
+    print(pred_date)
+    test_X = X = zeros((1, 45))
+    for i in range(0, 29):
+        test_X[0][i+1] = train_X[-1][i]
+    test_X[0][0] = train_Y [-1][0]
+    test_X[0][30] = 0
+    test_X[0][31] = train_X[-1][31]-1
+    test_X[0][32] = train_X[-1][32]+1
+    col = int(pred_date.month)
+    test_X[0][32+col] = 1.0
+    ans = linearReg.predict(test_X.reshape(1, -1))[0][0]
+    print(test_X)
+    print(ans)
